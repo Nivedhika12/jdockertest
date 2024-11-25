fe4c87f254cf30c2bad14848a0dfed9545faaf72
Nivedhika <ashalanivedhika0812@gmail.com>
2024-11-25 15:57:04 +0530

    java Docker test

--- Array.java
+++ Array.java
@@ -1,7 +1,9 @@
-public class SortArray {
+import java.util.*;
+
+public class Array {
     public static void main(String[] args) {
         int[] arr = {5, 3, 8, 1, 2};
-        Arrays.sort(arr);
-        System.out.println("Sorted Array: " + Arrays.toString(arr));
+        Array.sort(arr);
+        System.out.println("Sorted Array: " + Array.toString(arr));
     }
 }
